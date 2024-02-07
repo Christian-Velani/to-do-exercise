116174a401d380799e9179bbb42d375f3e5e8595
Christian Velani <chris13082000@gmail.com>
2024-02-07 10:57:06 -0300

    Criei o header

---
+++ components/header.tsx
@@ -0,0 +1,18 @@
+import AppBar from '@mui/material/AppBar';
+import Image from 'next/image';
+import rocketImage from "/images/rocket.svg";
+import styles from "./header.module.css";
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <Image className={styles.image}
+                src={rocketImage}
+                width={22}
+                height={36}
+                alt='rocket' />
+            <p className={styles.preTitle}>to</p>
+            <p className={styles.posTitle}>do</p>
+        </header>
+    )
+}
