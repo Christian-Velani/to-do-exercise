e8c4c1b18fbc1c366327432b0d6a26256a4ca41d
Christian Velani <chris13082000@gmail.com>
2024-02-09 10:48:12 -0300

    Removendo código desnecessário

--- app/context.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { createContext } from "react";
-
-interface TaskData {
-    id: number;
-    texto: string;
-    status: boolean;
-}
-export const MyContext = createContext<string[]>([]);
